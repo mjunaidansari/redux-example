9e0cda5fa25f13ea6a9790a48fdf9b6fcf3d21dd
Junaid Ansari <junaidshahid.ansari@gmail.com>
2023-07-26 23:09:11 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,115 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+import React from "react"
+import ReactDOM from 'react-dom/client'
+
+import { createStore } from "redux"
+
+// const counterReducer = (state = 0, action) => {
+
+// 	switch(action.type) {
+// 		case 'INCREMENT':
+// 			return state + 1
+// 		case 'DECREMENT':
+// 			return state - 1
+// 		case 'ZERO':
+// 			return 0
+// 		default: 
+// 			return state
+// 	}
+
+// }
+
+// const store = createStore(counterReducer)
+
+// const App = () => {
+// 	return (
+// 		<div>
+// 			<div>
+// 				{store.getState()}
+// 			</div>
+// 			<button onClick={e => store.dispatch({type: 'INCREMENT'})}>
+// 				Plus
+// 			</button>
+// 			<button onClick={e => store.dispatch({type: 'DECREMENT'})}>
+// 				Minus
+// 			</button>
+// 			<button onClick={e => store.dispatch({type: 'ZERO'})}>
+// 				Zero
+// 			</button>
+// 		</div>
+// 	)
+// }
+
+const noteReducer = (state = [], action) => {
+
+	if(action.type === 'NEW_NOTE') {
+		state.concat(action.payload)
+		return state
+	}
+	return state
+
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+	type: 'NEW_NOTE',
+	payload: {
+		content: 'the app state is in redux store',
+		important: true,
+		id: 1
+	}
+})
+
+store.dispatch({
+	type: 'NEW_NOTE',
+	payload: {
+		content: 'state changes are mad with actions',
+		important: false,
+		id: 1
+	}
+})
+
+const App = () => {
+
+	return (
+		<div>
+			<ul>
+				{store.getState().map(note => 
+					<li key={note.id}>
+						{note.content} <strong>{note.important? 'important' : '' }</strong>
+					</li>
+				)}
+			</ul>
+		</div>
+	)
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+	console.log('changed')
+	root.render(<App/>)
+}
+
+renderApp()
+store.subscribe(renderApp)
