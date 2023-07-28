93ee42e30c2258b7a274572750569d4ed5ada01f
Junaid Ansari <junaidshahid.ansari@gmail.com>
2023-07-28 20:57:00 +0530

    separating GUI components into presentational and container components, and using useDispatch and useSelector hooks

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import React from "react"
-
-import { useSelector, useDispatch } from "react-redux"
-
-import { createNote, toggleImportanceOf } from "./reducers/noteReducer"
+import NewNote from "./components/NewNote"
+import Notes from "./components/Notes"
 
 const App = () => {
 
-	const dispatch = useDispatch()
-	const notes = useSelector(state => state)
-
-	const addNote = (event) => {
-		event.preventDefault()
-		const content = event.target.note.value
-		event.target.note.value = ''
-		dispatch(createNote(content))
-	}
-
-	const toggleImportance = (id) => {
-		dispatch(toggleImportanceOf(id))
-	}
-
 	return (
 		<div>
-			<form onSubmit={addNote}>
-				<input name="note"/>
-				<button type='submit'>Add</button>
-			</form>
-			<ul>
-				{notes.map(note => 
-					<li 
-						key = {note.id}
-						onClick = {() => toggleImportance(note.id)}
-					>
-						{note.content} <strong>{note.important ? 'important': ''}</strong>
-					</li>
-				)}
-			</ul>
+			<NewNote/>
+			<Notes/>
 		</div>
 	)
 
